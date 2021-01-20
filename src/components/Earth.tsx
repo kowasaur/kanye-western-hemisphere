2207edce2d69386cb225b988ef8c5291f461608a
kowasaur <kowasaur@gmail.com>
2021-01-20 20:20:13 +1000

    give earth a jolt

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -13,15 +13,25 @@ export const Earth: React.FC<EarthProps> = ({ isStopped }) => {
 
   const ref = useRef(null);
 
+  let speed = 0.003;
+
   useFrame(() => {
     if (!isStopped) {
       // @ts-ignore
-      ref.current.rotation.y += 0.003;
+      ref.current.rotation.y += speed;
     }
   });
 
+  function jolt() {
+    speed = 0.3;
+    setTimeout(() => {
+      speed = 0.003;
+    }, 400);
+  }
+
   return (
-    <Sphere args={[2, 69, 69]} castShadow ref={ref}>
+    // @ts-ignore
+    <Sphere args={[2, 69, 69]} castShadow ref={ref} onClick={jolt}>
       <meshStandardMaterial attach="material" metalness={0.1} map={earthTexture} />
     </Sphere>
   );
