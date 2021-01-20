ef4a2f8f03f2eeb6b20791bca1247e3a0894886d
kowasaur <kowasaur@gmail.com>
2021-01-20 20:07:12 +1000

    start/stop button

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
-import { OrbitControls, Plane, Sphere, Stars, useTexture } from "drei";
-import React, { Suspense } from "react";
+import { OrbitControls, Stars } from "drei";
+import React, { Suspense, useState } from "react";
 import { Canvas } from "react-three-fiber";
 import "./App.css";
 
-import plane from "./images/plane.png";
 import { Earth } from "./components/Earth";
 
 function App() {
-  const planeTexture = useTexture(plane);
+  const [isStopped, setIsStopped] = useState(false);
 
   return (
     <>
       <Canvas shadowMap>
         <ambientLight intensity={0.4} />
         <directionalLight intensity={1.5} castShadow position={[5, 3, 5]} />
-        {/* <pointLight position={[-10, 0, 20]} intensity={0.5} />
-        <pointLight position={[0, -10, 0]} intensity={1} /> */}
 
         <Stars />
-        {/* <Plane
-          args={[100, 100]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          position={[0, -10, 0]}
-          receiveShadow
-        >
-          <meshStandardMaterial attach="material" map={planeTexture} color="grey" />
-        </Plane> */}
+
         <Suspense fallback={null}>
-          <Earth />
+          <Earth isStopped={isStopped} />
         </Suspense>
 
         <OrbitControls />
       </Canvas>
+
+      <aside>
+        <button onClick={() => setIsStopped(!isStopped)}>{isStopped ? "start" : "stop"}</button>
+      </aside>
     </>
   );
 }
--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -4,14 +4,21 @@ import { useFrame } from "react-three-fiber";
 
 import earth from "../images/earth.png";
 
-interface EarthProps {}
+interface EarthProps {
+  isStopped: boolean;
+}
 
-export const Earth: React.FC<EarthProps> = ({}) => {
+export const Earth: React.FC<EarthProps> = ({ isStopped }) => {
   const earthTexture = useTexture(earth);
 
   const ref = useRef(null);
-  // @ts-ignore
-  useFrame(() => (ref.current.rotation.y += 0.003));
+
+  useFrame(() => {
+    if (!isStopped) {
+      // @ts-ignore
+      ref.current.rotation.y += 0.003;
+    }
+  });
 
   return (
     <Sphere args={[2, 69, 69]} castShadow ref={ref}>
